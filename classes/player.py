05769f3937f5684f4d0ff5adcef75ba8b809c35b
Vincent Lee <vlee@vlee.me.uk>
2021-04-20 00:18:26 +0100

    Move player data out to own file

---
+++ classes/player.py
@@ -0,0 +1,26 @@
+from .gameData import HeroData
+
+
+class PlayerStats:
+    """
+    Stores data of players character and their current stats
+    """
+
+    def __init__(self, HP: int, shield: int, speed: int, speed_length: int):
+        self.HP = HP
+        self.shield = shield
+        self.speed = speed
+        self.speed_length = speed_length
+
+
+class Player:
+    """
+    Stores data on a player
+    """
+
+    def __init__(self, username: str, user_id: str, hero: HeroData,
+                 hp: int, shield: int, speed: int, speed_length: int):
+        self.username = username
+        self.user_id = user_id
+        self.hero = hero
+        self.stats = PlayerStats(hp, shield, speed, speed_length)
